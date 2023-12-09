2ca3a8a9b7d306883b758e5db77375b2df77f3ea
Johan Rosa <johan.rosaperez@gmail.com>
2023-12-09 18:29:01 -0400

    Drag and drop

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const gameBoard = document.querySelector("#gameboard");
 const playerDisplay = document.querySelector("#player");
-const infoDisplay = document.querySelector("#info-display")
+const infoDisplay = document.querySelector("#info-display");
 
-const width = 8
+const width = 8;
+let playerGo = "black";
+playerDisplay.textContent = playerGo;
 
 const startPices = [
     rook, knight, bishop, king, queen, bishop, knight, rook,
@@ -19,7 +21,9 @@ function createBoard() {
     startPices.forEach((startPice, i) => {
        const square =  document.createElement('div');
        square.classList.add('square');
-       square.setAttribute("quare-id", i);
+       square.innerHTML = startPice;
+       square.firstChild?.setAttribute("draggable", true)
+       square.setAttribute("square-id", i);
 
        const row = Math.floor((63 - i) / 8) + 1;
 
@@ -29,9 +33,59 @@ function createBoard() {
         square.classList.add(i % 2 === 0 ? "brown" : "beige")
        }
 
-       square.innerHTML = startPice;
+       if (i <= 15) {
+        square.firstChild.firstChild.classList.add("black")
+       }
+       if (i >= 48) {
+        square.firstChild.firstChild.classList.add("white")
+       }
+
        gameBoard.append(square)
     })
 }
 
-createBoard()
+createBoard();
+
+
+
+
+
+const allSquares = document.querySelectorAll("#gameboard .square");
+
+
+let startPosition
+let draggedElement
+
+function dragStart (e) {
+    startPosition = e.target.parentNode.getAttribute("square-id")
+    draggedElement = e.target
+}
+
+function dragOver (e) {
+    e.preventDefault()
+ }
+
+ function dragDrop (e) {
+    e.stopPropagation()
+    const taken = e.target.classList.contains("piece")
+
+    //e.target.parentNode.append(draggedElement)
+    //e.target.remove()
+    //e.target.append(draggedElement)
+    changePlayer()
+ }
+
+function changePlayer () {
+    playerGo = playerGo == "black" ? "white" : "balck"
+    playerDisplay.textContent = playerGo;
+}
+
+allSquares.forEach((square) => {
+    square.addEventListener("dragstart", dragStart)
+    square.addEventListener("dragover", dragOver)
+    square.addEventListener("drop", dragDrop)
+}) 
+
+function reverseIds () {
+    document
+}
